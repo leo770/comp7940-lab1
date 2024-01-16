65b50f6167fe4f5a7effe591fdbbd20dff2bbed1
leo770 <liwenhao507@gmail.com>
2024-01-16 21:07:07 +0800

    Thrid commit

---
+++ ex2.py
@@ -0,0 +1,13 @@
+# Write a function that prints all factors of the given parameter x
+l = [52633, 8137, 1024, 999]
+def print_factor(x):
+# your code here
+	for i in range(x+1):
+	# your code here
+	if i == 0 :
+		continue
+	if x % i == 0 :
+		print(i)
+
+for t in l:
+	print_factor(t)
