43262191531a7f02cf2cef3601eb2f9848c9fd7f
leo770 <liwenhao507@gmail.com>
2024-01-16 21:08:13 +0800

    Thrid commit

--- ex2.py
+++ ex2.py
@@ -3,11 +3,10 @@ l = [52633, 8137, 1024, 999]
 def print_factor(x):
 # your code here
 	for i in range(x+1):
-	# your code here
-	if i == 0 :
-		continue
-	if x % i == 0 :
-		print(i)
+		if i == 0 :
+			continue
+		if x % i == 0 :
+			print(i)
 
 for t in l:
 	print_factor(t)
