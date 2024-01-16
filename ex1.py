1914f910d3fa680159aea36f506b12bfab1cbdc8
leo770 <liwenhao507@gmail.com>
2024-01-16 20:59:33 +0800

    Second commit

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Find the all factors of x using a loop and the operator %
+# % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
+x = 52633
+for i in range(x+1):
+	# your code here
+	if i == 0 :
+		continue
+	if x % i == 0 :
+		print(i)
+
